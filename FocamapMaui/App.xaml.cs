00f374315c925e8c9661fe80dd4b658ba4d34f50
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-26 16:59:52 -0300

    Initial Commit
    Initial Commit

---
+++ FocamapMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FocamapMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ FocamapMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FocamapMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
