65f53d3a0b79d6bf2480791ecc1cf69dbc74fb65
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-26 19:31:10 -0300

    Create ContentPageBase
    Create ContentPageBase

---
+++ FocamapMaui/MVVM/Base/ContentPageBase.cs
@@ -0,0 +1,12 @@
+﻿namespace FocamapMaui.MVVM.Base
+{
+    public class ContentPageBase : ContentPage
+	{
+		public ContentPageBase()
+		{
+			Shell.SetNavBarIsVisible(this, false);			
+			Content = new Grid();			
+		}
+	}
+}
+
