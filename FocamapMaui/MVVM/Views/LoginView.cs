31b69592f3875e0055a94fdd691ac7de490d6879
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-26 19:32:18 -0300

    Creating new LoginView Initial
    Creating new LoginView Initial

---
+++ FocamapMaui/MVVM/Views/LoginView.cs
@@ -0,0 +1,27 @@
+﻿using FocamapMaui.Controls.Resources;
+using FocamapMaui.MVVM.Base;
+
+namespace FocamapMaui.MVVM.Views
+{
+    public class LoginView : ContentPageBase
+	{
+		public LoginView()
+		{
+			BackgroundColor = ControlResources.GetResource<Color>("CLPrimary");
+
+            Content = new VerticalStackLayout()
+			{				
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+				Children =
+				{
+					new Label
+					{
+						Text = "Login"
+					}
+				}
+			};
+		}
+	}
+}
+
