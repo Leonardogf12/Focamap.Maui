a06afccf7ec1755a904cbf45d25738bee5ad0e77
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-27 17:11:57 -0300

    Change folder and create new components of Input
    Change folder and create new components of Input

---
+++ FocamapMaui/Components/UI/PrimaryButtonCustom.cs
@@ -0,0 +1,19 @@
+﻿using FocamapMaui.Controls.Resources;
+
+namespace FocamapMaui.Components.UI
+{
+    public class PrimaryButtonCustom : Button
+	{
+		public PrimaryButtonCustom(string text, string textColor, string backgroundColor)
+		{
+            Text = text;                           
+            BackgroundColor = ControlResources.GetResource<Color>(backgroundColor);
+            TextColor = ControlResources.GetResource<Color>(textColor);
+            HeightRequest = 45;
+            CornerRadius = 30;
+            FontSize = 16;           
+            FontFamily = "MontserratSemibold";
+        }
+	}
+}
+
---
+++ FocamapMaui/Components/UI/PrimaryEmptyButtonCustom.cs
@@ -0,0 +1,20 @@
+﻿using FocamapMaui.Controls.Resources;
+
+namespace FocamapMaui.Components.UI
+{
+    public class PrimaryEmptyButtonCustom : Button
+    {
+        public PrimaryEmptyButtonCustom(string text, string textColor, string borderColor)
+        {
+            Text = text;
+            BackgroundColor = Colors.Transparent;
+            BorderColor = ControlResources.GetResource<Color>(borderColor);
+            TextColor = ControlResources.GetResource<Color>(textColor);
+            HeightRequest = 45;
+            CornerRadius = 30;
+            FontSize = 16;
+            FontFamily = "MontserratSemibold";
+        }
+    }
+}
+
