5c4e2401968584ae3c93991a1d74a508b16e9f04
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-27 17:11:33 -0300

    Add Components basic
    Add Components basic

---
+++ FocamapMaui/Components/UI/Basics/CommomBasic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FocamapMaui.Components.UI.Basics
+{
+	public static class CommomBasic
+	{
+        public static StackLayout GetStackLayoutBasic(int spacing = 15, bool useMargin = false)
+        {
+            return new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Spacing = spacing,
+                Margin = useMargin ? new Thickness(0, 0, 0, 50) : 0
+            };
+        }
+    }
+}
+
