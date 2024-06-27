60013b7b3f509df4a52a2d72ac08d0f2ec96ca63
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-27 17:10:47 -0300

    Create Navigation Service
    Create Navigation Service

---
+++ FocamapMaui/Services/Navigation/INavigationService.cs
@@ -0,0 +1,8 @@
+﻿namespace FocamapMaui.Services.Navigation
+{
+    public interface INavigationService
+	{
+        Task NavigationWithParameter<T>(IDictionary<string, object> parameter = null, View view = null) where T : IView;
+    }
+}
+
---
+++ FocamapMaui/Services/Navigation/NavigationService.cs
@@ -0,0 +1,37 @@
+﻿namespace FocamapMaui.Services.Navigation
+{
+	public class NavigationService : INavigationService
+	{
+		private bool _isBrowsing;
+
+        public async Task NavigationWithParameter<T>(IDictionary<string, object> parameter = null, View view = null) where T : IView
+		{
+            if (_isBrowsing) return;
+
+            try
+            {
+                _isBrowsing = true;
+
+                var typeView = typeof(T);
+
+                if (parameter is not null)
+                {
+                    await Shell.Current.GoToAsync(typeView.Name, parameter);
+                }
+                else
+                {
+                    await Shell.Current.GoToAsync($"{typeView.Name}");
+                }
+            }
+            catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+                _isBrowsing = false;
+            }						
+		}
+	}
+}
+
