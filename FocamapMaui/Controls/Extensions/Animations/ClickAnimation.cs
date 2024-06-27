73784cdb4f7f416dfa03f41608b758a9c228fdce
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-27 14:35:04 -0300

    Add new Extensions Classes
    ClickAnimation - FadeAnimation()
    ControlEvents - AddTapGesture()

---
+++ FocamapMaui/Controls/Extensions/Animations/ClickAnimation.cs
@@ -0,0 +1,13 @@
+﻿namespace FocamapMaui.Controls.Extensions.Animations
+{
+    public static class ClickAnimation
+	{
+		public static async Task FadeAnimation(this View element)
+		{
+			await element.FadeTo(opacity: 0.5);
+            await Task.Delay(25);
+            await element.FadeTo(opacity: 1, 100);
+        }
+    }
+}
+
---
+++ FocamapMaui/Controls/Extensions/Events/ControlEvents.cs
@@ -0,0 +1,18 @@
+﻿namespace FocamapMaui.Controls.Extensions.Events
+{
+    public static class ControlEvents
+	{
+        public static void AddTapGesture(this View element, EventHandler<TappedEventArgs> tappedEventHandler)
+        {
+            var tapGestureRecognizer = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 1
+            };
+
+            tapGestureRecognizer.Tapped += tappedEventHandler;
+
+            element.GestureRecognizers.Add(tapGestureRecognizer);
+        }       
+    }
+}
+
