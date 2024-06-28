b40138027ca1f140915cbe7f77ce9bb7b4105b67
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-28 10:38:43 -0300

    Add ViewMocelBase

---
+++ FocamapMaui/MVVM/Base/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FocamapMaui.MVVM.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+	{
+        public ViewModelBase()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
+
