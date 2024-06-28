6564d2b8dbcb71cdc00f5664d81afe3679a2c756
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-28 15:16:16 -0300

    ViewModel to HomeMapView

---
+++ FocamapMaui/MVVM/ViewModels/HomeMapViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.ObjectModel;
+using FocamapMaui.MVVM.Base;
+using Microsoft.Maui.Controls.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+
+namespace FocamapMaui.MVVM.ViewModels
+{
+    public class HomeMapViewModel : ViewModelBase
+	{
+        #region Properties
+
+        private ObservableCollection<Pin> _pinsList;
+        public ObservableCollection<Pin> PinsList
+        {
+            get => _pinsList;
+            set
+            {
+                _pinsList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Map _map;
+        public Map Map
+        {
+            get => _map;
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+                UpdateMapPins();
+            }
+        }
+
+        #endregion
+
+
+        public HomeMapViewModel()
+		{
+            LoadPins();
+        }
+
+        private void LoadPins()
+        {
+            var list = new List<Pin>
+            {
+                new Pin
+                {
+                    Label = "Assalto",
+                    Address = "Armado?:Sim | Feridos?:1",
+                    Type = PinType.Generic,
+                    Location = new Location(-19.394837, -40.049279),
+                },
+                new Pin
+                {
+                    Label = "Tiros",
+                    Address = "Feridos?:1",
+                    Type = PinType.Place,
+                    Location = new Location(-19.391254, -40.050202)
+                },
+                new Pin
+                {
+                    Label = "Tiros",
+                    Address = "Feridos?:Não",
+                    Type = PinType.SearchResult,
+                    Location = new Location(-19.395747, -40.037993)
+                },
+                new Pin
+                {
+                    Label = "Assalto",
+                    Address = "Armado?:Sim | Feridos?:Não",
+                    Type = PinType.SavedPin,
+                    Location = new Location(-19.400564, -40.045224)
+                },
+            };
+
+            PinsList = new ObservableCollection<Pin>(list);
+        }
+
+        public void UpdateMapPins()
+        {
+            if (Map == null || PinsList == null)
+                return;
+
+            Map.Pins.Clear();
+
+            foreach (var pin in PinsList)
+            {
+                Map.Pins.Add(pin);
+            }
+        }
+
+    }
+}
+
