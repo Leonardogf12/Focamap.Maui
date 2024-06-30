bb8a92bfd6bc98351e13064575c9ef7898a5efa7
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-30 13:28:11 -0300

    Start new UI component
    BottomSheetAddOccurrenceCustom

---
+++ FocamapMaui/Components/UI/BottomSheetAddOccurrenceCustom.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Maui.Controls;
+using FocamapMaui.Controls.Resources;
+
+namespace FocamapMaui.Components.UI
+{
+    public class BottomSheetAddOccurrenceCustom : BottomSheet
+    {
+		public BottomSheetAddOccurrenceCustom()
+		{
+			BackgroundColor = ControlResources.GetResource<Color>("CLSecondary");
+
+			Content = BuildBottomSheet;
+		}
+
+		private View BuildBottomSheet
+		{
+			get
+			{				
+				return new Grid();
+			}
+		}
+
+    }
+}
+
