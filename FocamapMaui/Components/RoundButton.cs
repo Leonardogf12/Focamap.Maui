a01131033fcc1482197daf2e40f2d1fc6b4e66e9
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-30 13:27:54 -0300

    Create new UI component
    RoundButton

---
+++ FocamapMaui/Components/RoundButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+using FocamapMaui.Controls.Resources;
+
+namespace FocamapMaui.Components
+{
+    public static class RoundButton
+    {
+		public static Button GetRoundButton(string iconName = "", EventHandler eventHandler = null)
+		{
+            var roundButton = new Button
+            {
+                ImageSource = ImageSource.FromFile(iconName),              
+                BackgroundColor = ControlResources.GetResource<Color>("CLBlack"),                
+                HeightRequest = 60,
+                WidthRequest = 60,
+                CornerRadius = 60,                
+            };
+           
+            if (eventHandler is not null)
+            {
+                roundButton.Clicked += eventHandler;
+            }
+            
+            return roundButton;
+        }        
+    }
+}
+
