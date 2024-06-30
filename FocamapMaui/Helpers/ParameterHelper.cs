f6f99e7f3715a000467405889f270ac8b0aa66fa
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-30 13:27:40 -0300

    Create new Helper
    ParameterHelper

---
+++ FocamapMaui/Helpers/ParameterHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace FocamapMaui.Helpers
+{
+    public static class ParameterHelper
+	{
+        public static Dictionary<string, object> SetParameter(string keyString, bool valueObject)
+        {
+            return new Dictionary<string, object>
+            {
+                { keyString , valueObject }
+            };
+        }
+    }
+}
+
