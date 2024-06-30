ec2b2aeab6c7239699b5b67dd6799c5b2b7b7604
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-30 13:27:31 -0300

    Create new Empty Pages
    OccurrencesHistoryView
    UserDetailView

---
+++ FocamapMaui/MVVM/Views/OccurrencesHistoryView.cs
@@ -0,0 +1,36 @@
+﻿using FocamapMaui.Controls.Resources;
+using FocamapMaui.MVVM.Base;
+
+namespace FocamapMaui.MVVM.Views
+{
+    public class OccurrencesHistoryView : ContentPageBase
+	{
+		public OccurrencesHistoryView()
+		{
+			BackgroundColor = ControlResources.GetResource<Color>("CLSecondary");
+
+			Content = BuildOccurrencesHistoryView;
+        }
+
+        public View BuildOccurrencesHistoryView
+		{
+			get
+			{
+				return new VerticalStackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					Children =
+					{
+						new Label
+						{
+							FontSize = 12,
+							Text = "Em construção - OccurrencesHistoryView"
+                        }
+					}
+				};
+			}
+		}
+    }
+}
+
---
+++ FocamapMaui/MVVM/Views/UserDetailView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FocamapMaui.Controls.Resources;
+using FocamapMaui.MVVM.Base;
+
+namespace FocamapMaui.MVVM.Views
+{
+	public class UserDetailView : ContentPageBase
+	{
+		public UserDetailView()
+		{
+			BackgroundColor = ControlResources.GetResource<Color>("CLSecondary");
+			Content = BuildUserDetailView;
+        }
+
+		public View BuildUserDetailView
+		{
+			get
+			{
+                return new VerticalStackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Em construção - UserDetailView"
+                        }
+                    }
+                };
+            }
+		}
+
+    }
+}
+
