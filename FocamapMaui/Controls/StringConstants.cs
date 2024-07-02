813ced49eb02abf302ea064294facd08de2407e3
leonardofazolo <leonardogf.contato@gmail.com>
2024-07-02 14:56:13 -0300

    Create stringConstants class

---
+++ FocamapMaui/Controls/StringConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace FocamapMaui.Controls
+{
+    public static class StringConstants
+	{
+		public const string ES = "ES";
+        public const string MG = "MG";
+
+        public const string FIREBASE_AUTH_TOKEN_KEY = "FirebaseAuthToken";
+        public const string FIREBASE_USER_LOCAL_ID_KEY = "FirebaseUserLocalIdKey";
+        public const string FIREBASE_AUTH_PROVIDER_KEY = "AIzaSyBmEIGJpn6VnoMIXxgQTgFxvybI9xPO_gE";
+
+        public const string HOMEMAPVIEW_ROUTE = "//HomeMapView";
+        public const string LOGINVIEW_ROUTE = "//LoginView";
+    }
+}
+
