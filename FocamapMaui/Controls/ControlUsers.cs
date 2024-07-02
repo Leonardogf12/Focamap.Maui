850c759b60817a6ab1afe56b933444e18a853774
leonardofazolo <leonardogf.contato@gmail.com>
2024-07-02 14:57:08 -0300

    Create ControlUsers to helper classes

---
+++ FocamapMaui/Controls/ControlUsers.cs
@@ -0,0 +1,22 @@
+﻿namespace FocamapMaui.Controls
+{
+    public static class ControlUsers
+	{
+        public static void SetLocalIdByUserLogged()
+        {
+            App.UserLocalIdLogged = ControlPreferences.GetKeyOfPreferences(StringConstants.FIREBASE_USER_LOCAL_ID_KEY);
+        }
+
+        public static async void CheckUserHasLogged()
+        {
+            var value = ControlPreferences.GetKeyOfPreferences(StringConstants.FIREBASE_AUTH_TOKEN_KEY);
+
+            if (string.IsNullOrEmpty(value)) return;
+
+            SetLocalIdByUserLogged();
+
+            await Shell.Current.GoToAsync(StringConstants.HOMEMAPVIEW_ROUTE);         
+        }
+    }
+}
+
