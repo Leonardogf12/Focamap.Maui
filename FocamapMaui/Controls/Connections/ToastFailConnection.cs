42df5e179a9f05ca0e92e7cfa75531f34c040b46
leonardofazolo <leonardogf.contato@gmail.com>
2024-07-02 14:55:27 -0300

    Create Toast of Connection Fail
    Create Toast of Connection Fail

---
+++ FocamapMaui/Controls/Connections/ToastFailConnection.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace FocamapMaui.Controls.Connections
+{
+    public static class ToastFailConnection
+	{
+        public static bool CheckIfConnectionIsSuccessful()
+        {
+            NetworkAccess access = Connectivity.Current.NetworkAccess;
+
+            return access == NetworkAccess.Internet;
+        }
+
+        public async static void ShowToastMessageFailConnection()
+        {
+            CancellationTokenSource cancellationTokenSource = new();
+
+            ToastDuration duration = ToastDuration.Long;
+            var toast = Toast.Make("Verifique sua conexão com a internet", duration, 14);
+
+            await toast.Show(cancellationTokenSource.Token);
+        }
+    }
+}
+
