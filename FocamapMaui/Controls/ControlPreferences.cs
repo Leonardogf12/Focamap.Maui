0c7aec02f63cccd74804bebdf2b88c2963d9ceef
leonardofazolo <leonardogf.contato@gmail.com>
2024-07-02 14:55:51 -0300

    Create Class control from save preferences in memory
    Create Class control from save preferences in memory

---
+++ FocamapMaui/Controls/ControlPreferences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace FocamapMaui.Controls
+{
+	public static class ControlPreferences
+	{
+        public static string GetKeyOfPreferences(string key)
+        {
+            return Preferences.Get(key, "");
+        }
+
+        public static void AddKeyOnPreferences(string key, object contentOfObject)
+        {
+            var serializeContent = JsonConvert.SerializeObject(contentOfObject);
+
+            Preferences.Set(key, serializeContent);
+        }
+
+        public static void RemoveKeyFromPreferences(string key)
+        {
+            if (Preferences.ContainsKey(key))
+            {
+                Preferences.Remove(key);
+            }
+        }       
+    }
+}
+
