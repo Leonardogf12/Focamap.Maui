452ede0f226e522d433a3387c20a9ca232b2c0a7
leonardofazolo <leonardogf.contato@gmail.com>
2024-07-02 14:56:22 -0300

    Create OccurrenceModel

---
+++ FocamapMaui/MVVM/Models/OccurrenceModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FocamapMaui.MVVM.Models
+{
+    public class OccurrenceModel
+	{
+        public int Id { get; set; }
+
+		public string Address { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public TimeSpan Hour { get; set; }
+
+		public string Resume { get; set; }
+	}
+}
+
